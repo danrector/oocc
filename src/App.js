0bdfb06e1ff16209f1783645d5ae3a4edfe5aa19
Daniel Rector <rector.danielk@gmail.com>
2018-03-16 20:01:45 -0400

    added flexbox

--- src/App.js
+++ src/App.js
@@ -31,30 +31,32 @@ export default class LightboxExample extends Component {
       <div>
         <h1>OUT OF CONTEXT COMICS</h1>
 
-        <img className='comic' src='http://i.imgur.com/Ohuu7bI.jpg?1'
-          onClick={() => this.setState({ photoIndex: 0, isOpen: true })}
-          alt='comic'></img>
-        <img className='comic' src='http://i.imgur.com/dZoFSAo.jpg?1'
-          onClick={() => this.setState({ photoIndex: 1, isOpen: true })}
-          alt='comic'></img>
-        <img className='comic' src='http://i.imgur.com/tEQAwsG.jpg?1'
-          onClick={() => this.setState({ photoIndex: 2, isOpen: true })}
-          alt='comic'></img>
-        <img className='comic' src='https://i.imgur.com/DUB7A3y.jpg?1'
-          onClick={() => this.setState({ photoIndex: 3, isOpen: true })}
-          alt='comic'></img>
-        <img className='comic' src='https://i.imgur.com/PTzeZub.jpg?1'
-          onClick={() => this.setState({ photoIndex: 4, isOpen: true })}
-          alt='comic'></img>
-        <img className='comic' src='https://i.imgur.com/Q0eyxeC.jpg?1'
-          onClick={() => this.setState({ photoIndex: 5, isOpen: true })}
-          alt='comic'></img>
-        <img className='comic' src='https://i.imgur.com/oQwVmBZ.jpg?1'
-          onClick={() => this.setState({ photoIndex: 6, isOpen: true })}
-          alt='comic'></img>
-        <img className='comic' src='https://i.imgur.com/WZk2jFd.jpg?1'
-          onClick={() => this.setState({ photoIndex: 7, isOpen: true })}
-          alt='comic'></img>
+        <div className='comicContainer'>
+          <img className='comic' src='http://i.imgur.com/Ohuu7bI.jpg?1'
+            onClick={() => this.setState({ photoIndex: 0, isOpen: true })}
+            alt='comic'></img>
+          <img className='comic' src='http://i.imgur.com/dZoFSAo.jpg?1'
+            onClick={() => this.setState({ photoIndex: 1, isOpen: true })}
+            alt='comic'></img>
+          <img className='comic' src='http://i.imgur.com/tEQAwsG.jpg?1'
+            onClick={() => this.setState({ photoIndex: 2, isOpen: true })}
+            alt='comic'></img>
+          <img className='comic' src='https://i.imgur.com/DUB7A3y.jpg?1'
+            onClick={() => this.setState({ photoIndex: 3, isOpen: true })}
+            alt='comic'></img>
+          <img className='comic' src='https://i.imgur.com/PTzeZub.jpg?1'
+            onClick={() => this.setState({ photoIndex: 4, isOpen: true })}
+            alt='comic'></img>
+          <img className='comic' src='https://i.imgur.com/Q0eyxeC.jpg?1'
+            onClick={() => this.setState({ photoIndex: 5, isOpen: true })}
+            alt='comic'></img>
+          <img className='comic' src='https://i.imgur.com/oQwVmBZ.jpg?1'
+            onClick={() => this.setState({ photoIndex: 6, isOpen: true })}
+            alt='comic'></img>
+          <img className='comic' src='https://i.imgur.com/WZk2jFd.jpg?1'
+            onClick={() => this.setState({ photoIndex: 7, isOpen: true })}
+            alt='comic'></img>
+        </div>
 
 
         {isOpen && (
